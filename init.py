3255d08450216915ecd89f59bed7aedf85671944
Yatharth <yatharth1997@gmail.com>
2022-01-27 15:14:25 +0530

    Minor changes

--- init.py
+++ init.py
@@ -16,39 +16,58 @@ Objects in study:
 
 Modules:
 	• Rules : Set of functions that select the agents
+
+
+
+• Coordinate system for side -> n X m matrix
+• Write Rule class 
+
+• 
 """
+
+
+
+class Position:
+
+	pos_ss = [0, 1, 2, 3] # Position State Space
+
+	def __init__(self):
+		self.pos = pos_ss[0]
+
+	def read(self):
+		return self.pos.pos
+
+	def __repr__():
+		return f"[[{court_pos} - {side_pos} ]]"
+
+
+
 
 class Shuttle:
 
-	pos_ss = [0, 1, 2, 3] # Position State Space
-	activity_ss = [False, True] # Active or inactive
+	# Why tupple?
+	pos_ss = (0, 1, 2, 3) # Position State Space
+	activity_ss = (False, True) # Active or inactive
+
 
 	def __init__(self):
-		self.position = self.pos_ss[0] # Possible positions → [0, 1, 2, 3]
+		self.pos = Position() # Possible positions → [0, 1, 2, 3]
 
-		self.activity = activity_ss[0]
-
-
-
-
-
+		self.activity = self.activity_ss[0]
 
 
 	# For Completeness only ↓
-
 	def toggle(self):
 		"""	
 		Activity state of Shuttle is toggled.
 		"""
-		score = 1 * self.activity
 		self.activity = not self.activity
-		return score
 
 	def maintained(self):
 		"""
 		Activity state of Shuttle is maintained.
 		"""
-		return None
+		pass
 
 
 class Player:
@@ -59,17 +78,18 @@ class Player:
 		if side not in [0,1]:
 			raise Exception("Invalid side given to player!")
 
-		if tam_id not in [0,1]:
+		if team_id not in [0,1]:
 			raise Exception("Invalid team_id given to player!")
 
 		self.ability = ability # Ability of the player (defined as float in [0, 1])
+		self.affinity = self.ability
+
 		self.misses = self.miss_ratio() # Fraction of shots missed
 
-		self.court_pos_ss = [0,1] + side # State space in court position
+		#self.court_pos_ss = [0,1] + side # State space in court position
 
 		self.side_pos = team_id # Position of player (side coordinates)
 		self.court_pos = side[team_id] # Position of player (court coordinates)
-
 
 
 
@@ -94,8 +114,8 @@ class Court:
 		self.side0 = [ Player(side=0, team_id=0, ability=0.5),
 					   Player(side=0, team_id=1, ability=0.5)]
 
-		self.side1 = Player(side=1, team_id=0, ability=0.5),
-					 Player(side=1, team_id=1, ability=0.5) ] 
+		self.side1 = [ Player(side=1, team_id=0, ability=0.5),
+					   Player(side=1, team_id=1, ability=0.5) ] 
 		# Index of list does not reflect actual position of player
 		self.sides = [self.side0, self.side1]
 		self.score = [0, 0] # Score of both the teams
@@ -109,20 +129,28 @@ class Court:
 			"""
 
 			# Select Service Side
-			playerA = Rule.serve_selector()
+			playerA = Rule.serve()
+			playerB =Rule.serve_receiver()
+			
 			side = Rule.side_selector()
 			side = not side # ugly TODO: Fix ugly
 			# Treat Service as a shot 
+			shuttle.in_play = True
 
 			# Shuttle block
-			while self.shuttle.is_active():
+			while self.shuttle.in_play:
 				"""
 				Side is toggled until the shuttle is active.
 				"""
 				self.sides[not side][playerA].play(self.shuttle)
-				playerB = Rule.player_selector()
-				self.sides[not side][playerB]
-
-				playerA = Rule.player_selector()
+				playerB = Rnd.
 
 
+
+
+class Rule:
+	"""
+	Collection of functions that take in `court pos`, `side pos`, `score`.
+
+	Generates 
+	"""
